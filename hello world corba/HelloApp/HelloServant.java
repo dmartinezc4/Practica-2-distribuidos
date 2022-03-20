3cfa4c39eb3f5d5250596ceba40a96d6250338db
dmartinezc4 <43136464+dmartinezc4@users.noreply.github.com>
2022-03-20 10:33:13 +0100

    Código

---
+++ hello world corba/HelloApp/HelloServant.java
@@ -0,0 +1,9 @@
+package HelloApp;
+
+public class HelloServant extends HelloPOA{
+
+  //Este simplemente va a ejecutar la funcion del Hello World
+  public String sayHello(){
+    return "Hola desde el Java Server!";
+  }
+}
