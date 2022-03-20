65a20d26193d7ba19f55986e131bfe8bbe890ec0
dmartinezc4 <43136464+dmartinezc4@users.noreply.github.com>
2022-03-20 10:32:10 +0100

    Delete HelloServant.java

--- hello world corba/HelloServant.java
+++
@@ -1,9 +0,0 @@
-package HelloApp;
-
-public class HelloServant extends HelloPOA{
-
-  //Este simplemente va a ejecutar la funcion del Hello World
-  public String sayHello(){
-    return "Hola desde el Java Server!";
-  }
-}
